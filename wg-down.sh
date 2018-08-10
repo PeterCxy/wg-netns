e3941d309597760f16df6177039b396da758ad0e
Peter Cai <peter@typeblog.net>
2018-08-10 08:42:27 +0000

    initial commit

---
+++ wg-down.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CONFIG_NAME="$1"
+#DEV_NAME="$2"
+source /home/peter/wg/ext/$CONFIG_NAME.conf
+
+#ip netns exec $CONFIG_NAME ip link del dev $DEV_NAME
+#ip netns exec $CONFIG_NAME wg-quick down $CONFIG_NAME
+if $PRIVATE_VETH_ENABLED; then
+  ip netns exec $CONFIG_NAME ip link del dev "$CONFIG_NAME"1
+  ip link del dev "$CONFIG_NAME"0
+fi
+ip netns del $CONFIG_NAME
+
---
+++ wg-up.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+CONFIG_NAME="$1"
+DEV_NAME="wg-$CONFIG_NAME"
+source /home/peter/wg/ext/$CONFIG_NAME.conf
+
+ip netns add $CONFIG_NAME
+ip netns exec $CONFIG_NAME ip link set lo up
+ip link add dev $DEV_NAME type wireguard
+wg setconf $DEV_NAME /etc/wireguard/$CONFIG_NAME.conf
+ip link set $DEV_NAME netns $CONFIG_NAME up
+addrs=$(grep -oP "#Address = \K(.*)" /etc/wireguard/$CONFIG_NAME.conf)
+IFS=", "; for addr in $addrs; do
+  if [[ $addr = *":"* ]]; then
+    # IPv6
+    ip netns exec $CONFIG_NAME ip -6 addr add $addr dev $DEV_NAME
+  else
+    # IPv4
+    ip netns exec $CONFIG_NAME ip addr add $addr dev $DEV_NAME
+  fi
+done
+
+if $PRIVATE_VETH_ENABLED; then
+  ip link add dev "$CONFIG_NAME"0 type veth peer name "$CONFIG_NAME"1
+  ip link set "$CONFIG_NAME"0 up
+  ip link set "$CONFIG_NAME"1 netns $CONFIG_NAME up
+  ip addr add $PRIVATE_ADDRESS_HOST dev "$CONFIG_NAME"0
+  ip netns exec $CONFIG_NAME ip addr add $PRIVATE_ADDRESS_CLIENT dev "$CONFIG_NAME"1
+fi
+ip netns exec $CONFIG_NAME ip route add default dev $DEV_NAME
+ip netns exec $CONFIG_NAME ip -6 route add default dev $DEV_NAME
+
